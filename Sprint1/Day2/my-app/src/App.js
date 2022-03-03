285d86dbd5da4640c57c14d40f281f2a7a639ed6
Sanket Patil <patilsanket209@gmail.com>
2022-03-03 22:49:13 +0530

    completed assignment

---
+++ Sprint1/Day2/my-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+<h1>Mobile Operating System</h1>
+<div class="box">
+<ul>
+  <li> Android</li>
+  <li> Blackberry</li>
+  <li> Iphone</li>
+  <li> Windows Phone</li>
+</ul>
+</div>
+<div class="box">
+  <h1>Mobile Manufacturer</h1>
+<ul>
+  <li> Samsung</li>
+  <li> HTC</li>
+  <li> Micromax</li>
+  <li> Apple</li>
+</ul>
+</div>
+    </div>
+   
+  
+  );
+}
+
+export default App;
