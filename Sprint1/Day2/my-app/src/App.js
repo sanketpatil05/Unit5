049bf4aafcedbdf727830bf04de45bdf9117d964
Sanket Patil <patilsanket209@gmail.com>
2022-03-03 23:45:49 +0530

    made some changes

--- Sprint1/Day2/my-app/src/App.js
+++ Sprint1/Day2/my-app/src/App.js
@@ -2,30 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-<h1>Mobile Operating System</h1>
-<div class="box">
-<ul>
-  <li> Android</li>
-  <li> Blackberry</li>
-  <li> Iphone</li>
-  <li> Windows Phone</li>
-</ul>
-</div>
-<div class="box">
-  <h1>Mobile Manufacturer</h1>
-<ul>
-  <li> Samsung</li>
-  <li> HTC</li>
-  <li> Micromax</li>
-  <li> Apple</li>
-</ul>
-</div>
-    </div>
-   
-  
-  );
+  const list =[
+    {
+      "heading":"Mobile operating system",
+      "brand":["Andriod","Blackberry","iphone","WindowsPhone"]
+    },{
+      "heading":"Mobile Manufactures",
+      "brand":["Samsung","HTC","Micromax","Apple"]
+    }
+  ]
+  return <div>
+    {list.map((el)=>(
+      <Component data={el}/>
+    ))}
+
+  </div>
+  function Component({data}){
+    return (
+      <div>
+        <h1>{data['heading']}</h1>
+      {data['brand'].map((el)=>(
+        <li>{el}</li>
+      ))}
+
+      </div>
+    )
+  }
 }
 
 export default App;
